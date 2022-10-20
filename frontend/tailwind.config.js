b8e3c7194bfb569efe8552236b6f4f81f970d45e
Hburbach <hb4825@gmail.com>
2022-10-20 16:25:59 -0700

    installed react scripts to get react app to load properly

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontSize: {
+        14: '14px',
+      }},
+      width: {
+        400: '400px',
+        760: '760px',
+        780: '780px',
+        800: '800px',
+        1000: '1000px',
+        1200: '1200px',
+        1400: '1400px',
+      },
+      height: {
+        80: '80px',
+      },
+      minHeight: {
+        590: '590px',
+      },
+  },
+  plugins: [],
+  
+}
