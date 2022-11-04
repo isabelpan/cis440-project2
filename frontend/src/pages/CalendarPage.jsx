0694af35ad17dea4e6b811dd27e68461b596a83e
Isabel Pan <isabelpan@users.noreply.github.com>
2022-11-04 12:44:34 -0700

    gCal Sync

--- frontend/src/pages/CalendarPage.jsx
+++ frontend/src/pages/CalendarPage.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import { Navbar, Sidebar } from '../components';
 // import { Calendar } from '../components/calendar';
 import { googleCalendarSync } from '../components/calendar';
+import {Eventcalendar } from '@mobiscroll-react';
 
 const CalendarPage = () => {
+  const [myEvents, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    googleCalendarSync.init({
+      apiKey: 'APIKEY',
+      onInit: () => {
+        googleCalendarSync.getEvents(
+          'PUBLIC_CALENDAR',
+          new Date(2022,1,1),
+          new Date(2022, 3, 0),
+        ).then((events) => {
+          setEvents(events);
+        });
+      },
+    });
+  }, []);
+
   return (
     <div>
       <div>
